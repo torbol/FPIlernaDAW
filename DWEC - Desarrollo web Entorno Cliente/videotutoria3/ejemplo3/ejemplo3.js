32bd7ac279a9287746818823f435f26d5eea62c0
José Ángel <legnaes@gmail.com>
2024-10-24 17:16:12 +0200

    Se cambia html para que sea exáctamente igual al proporcionado por el ejercicio y se modifica el javascript para que interactue con este nuevo

--- DWEC - Desarrollo web Entorno Cliente/videotutoria3/ejemplo3/ejemplo3.js
+++ DWEC - Desarrollo web Entorno Cliente/videotutoria3/ejemplo3/ejemplo3.js
@@ -1,8 +1,12 @@
 /* Se nos pide ampliar el texto introducido en un input
 a partir de los botones + y - */
 
+// Declaramos las variables que contendrán nuestros botones y sobre las que ejecutaremos nuestras posteriores funciones
+var insertar = document.getElementById('insertar');
+var aumentar = document.getElementById('grande');
+var disminuir = document.getElementById('pequeno');
 
-// Creamos la función para cambiar el Texto de ejemplo 
+// Creamos la función para cambiar el Texto de ejemplo
 function changeExampleText() {
     // Obtenemos el texto que hemos introducido después de pulsar el botón Insertar
     let myText = document.getElementsByName("nombre"); // myText será un array de elementos, el elemento [0].value es nuestro texto
@@ -10,8 +14,8 @@ function changeExampleText() {
     document.getElementsByClassName("insertado")[0].innerHTML = myText[0].value;
 }
 
+// Función para aumentar el tamaño del texto
 function maximizeText() {
-    // Aumentamos el tamaño del texto
     let insertedText = document.getElementsByClassName("insertado")[0]; // Con esta variable accedemos a nuestro elemento Texto de ejemplo
     // Aumentamos de 10% en 10% el tamaño de nuestro texto
     let currentSizeText = insertedText.style.fontSize; // Variable local de la función para obtener el valor actual del tamaño de la fuente
@@ -19,11 +23,16 @@ function maximizeText() {
     insertedText.style.fontSize = updatedSize; // Actualizamos el valor de la fuente por el nuevo en el HTML
 }
 
+// Función para diminuir el tamaño del texto
 function minimizeText() {
-    // Disminuimos el tamaño del texto
     let insertedText = document.getElementsByClassName("insertado")[0]; // Con esta variable accedemos a nuestro elemento Texto de ejemplo
     // Disminuimos de 10% en 10% el tamaño de nuestro texto
     let currentSizeText = insertedText.style.fontSize; // Variable local de la función para obtener el valor actual del tamaño de la fuente
     let updatedSize = (parseFloat(currentSizeText) - 10) + "%"; // Variable local a la que restamos el 10% a la anterior
     insertedText.style.fontSize = updatedSize; // Actualizamos el valor de la fuente por el nuevo en el HTML
 }
+
+// Ejecutamos las correspondientes funciones cuando se presionan botones. Es decir, capturamos el evento onclick de cada botón cuando se produce.
+insertar.onclick = changeExampleText;
+aumentar.onclick = maximizeText;
+disminuir.onclick = minimizeText;
